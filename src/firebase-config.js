9552d6984fa298600b4e808e70b88a853204899a
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-07-02 20:19:01 +0300

    change firebase keys

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,12 +8,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyBDmS8dXsfq4D4-FiHx6JldvG8SeXqOdQA',
-    authDomain: 'iwindy-chat.firebaseapp.com',
-    projectId: 'iwindy-chat',
-    storageBucket: 'iwindy-chat.appspot.com',
-    messagingSenderId: '643394916700',
-    appId: '1:643394916700:web:b5c361907266f5328ff199',
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
